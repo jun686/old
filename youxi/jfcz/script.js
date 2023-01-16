ae1ff8f4d638a389bd6d93a641c4131dc1f76bed
jun686 <122136437+jun686@users.noreply.github.com>
2023-01-16 14:42:54 +0800

    html
    html

---
+++ youxi/jfcz/script.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bc58ca78d7b3819b67f142b17fe144a8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
